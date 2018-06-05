5b2a7ed3f1eded7ce7d706999bcb695f1d7e96f2
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-06-05 15:25:47 +0100

    Fixed bugs

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { ListGroup, ListGroupItem } from 'reactstrap';
-import ListItemCollapse from './listItemCollapse.js';
-
 
   class ListItem extends Component {
     componentDidMount() {
       var emailPath = require("./TEXT/"+ this.props.email['Control Number'] + ".txt");
-      var text = httpGet(emailPath);
-  console.log(text)
-  var containsPhoneNumber = phonenumber(text);
+      // var text = httpGet(emailPath);
+  // console.log(text)
 function phonenumber(inputtxt) {
 var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 if(inputtxt.value.match(phoneno)) {
@@ -20,20 +17,9 @@ else {
   return false;
 }
 }
-function httpGet(theUrl)
-{
-        var xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            return xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();
 }
-    }
+
+
 
 
   render() {
