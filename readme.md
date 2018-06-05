28b946d85b01134bfe519c2f59b1e934e016a47b
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-06-05 15:23:27 +0100

    Added html instead of jquery

---
+++ readme.md
@@ -0,0 +1,1 @@
+[hosted at](https://alexanderdavis.net/anexsys)
--- src/ListItem.js
+++ src/ListItem.js
@@ -7,7 +7,35 @@ import ListItemCollapse from './listItemCollapse.js';
   class ListItem extends Component {
     componentDidMount() {
       var emailPath = require("./TEXT/"+ this.props.email['Control Number'] + ".txt");
+      var text = httpGet(emailPath);
+  console.log(text)
+  var containsPhoneNumber = phonenumber(text);
+function phonenumber(inputtxt) {
+var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+if(inputtxt.value.match(phoneno)) {
+  return true;
+}
+else {
+  alert("message");
+  return false;
+}
+}
+function httpGet(theUrl)
+{
+        var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
     }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();
+}
+    }
+
+
   render() {
     return (
       <div>
